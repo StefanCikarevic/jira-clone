d0055bce03e8d7d9ea3c760fd6f399053feef947
StefanCikarevic <32402472+StefanCikarevic@users.noreply.github.com>
2024-10-20 18:25:54 +0200

    Init project

---
+++ src/features/test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button} from "@/components/ui/button";
+
+const Test = () => {
+    return (
+        <div className='text-red-400'>
+            hello
+            <Button>Hello</Button>
+        </div>
+    );
+};
+
+export default Test;
